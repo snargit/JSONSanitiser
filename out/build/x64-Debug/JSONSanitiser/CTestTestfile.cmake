6e8d9487c59dd8226ed574b579e17b9d518f98a3
snargit <dave.bailey@outlook.com>
2020-07-16 18:19:38 +0100

    Remove unnecessary files

--- out/build/x64-Debug/JSONSanitiser/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/repos/JSONSanitiser/JSONSanitiser
-# Build directory: D:/repos/JSONSanitiser/out/build/x64-Debug/JSONSanitiser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
