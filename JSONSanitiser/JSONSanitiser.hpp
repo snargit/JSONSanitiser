cb7127f204e7071fef890f4f5a721b77aaeebb8d
snargit <dave.bailey@outlook.com>
2020-07-19 14:32:47 +0100

    Update license

--- JSONSanitiser/JSONSanitiser.hpp
+++ JSONSanitiser/JSONSanitiser.hpp
@@ -1,4 +1,23 @@
-﻿#include "jsonsanitiser_export.h"
+﻿// Copyright (C) 2012 Google Inc.
+// Copyright (C) 2020 D. Bailey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// This is a port of the Java version of JSONSanitizer to C++ using UTF-8
+// as the character set. As far as possible the original source code has
+// been directly reused.
+
+#include "jsonsanitiser_export.h"
 
 #include <algorithm>
 #include <cstddef>
